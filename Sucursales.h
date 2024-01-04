bae285aede5c76dd7ba1f7e34f5e3caeee7065b2
LoncheDePierna <150973171+LoncheDePierna@users.noreply.github.com>
2024-01-04 10:39:11 -0600

    Update Sucursales.h

--- Sucursales.h
+++ Sucursales.h
@@ -1,8 +1,59 @@
 //Libreria Sucursales
-#ifndefine
-#define
+#include <iostream>
+#include <string>
+
+
+void Menu(){
+    int sucursal;
+    std::cout << std::endl << "======= INVENTARIO =======" << std::endl;
+    std::cout << "|                        |" << std::endl;    
+    std::cout << "|  Sucursal Aguilas (1)  |" << std::endl;     
+    std::cout << "|  Sucursal Fuentes (2)  |" << std::endl; 
+    std::cout << "|  Sucursal Arboles (3)  |" << std::endl; 
+    std::cout << "|                        |" << std::endl;
+    std::cout << "    Eligue Sucursal  ";
+    std::cin >> sucursal;
+    std::cout << "|                        |" << std::endl; 
+    std::cout << "==========================" << std::endl;
+    
+    
+    switch(sucursal){
+        case 1:{
+std::cout << "tilin insano";
+        }
+            break;
+        case 2:{
+std::cout << "tilin";
+        }
+            break;
+        case 3:{
+std::cout << "insano";
+        }
+            break;
+        default:
+            exit ( 0);
+    }    
+    
+};
 
 
 
 
-#endif
+
+int main(){
+Menu();  
+    
+
+    
+}
+
+
+
+
+//Inventarios
+//Sucursal 1 2 3
+
+//Cosa 1 (cantidad)
+//Cosa 2 (cantidad)
+//Cosa 3 (cantidad)
+//Cambiar Inventario
