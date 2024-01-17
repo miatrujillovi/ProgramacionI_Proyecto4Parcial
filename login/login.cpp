2c85fef0231d7aa5f922e7ad0d3064f4316b6c81
octavio <sergio.salcedo@uniat.edu.mx>
2024-01-17 16:51:56 -0600

    Creacion de estructura de login

--- login/login.cpp
+++ login/login.cpp
@@ -1,44 +1,47 @@
 #include <iostream>
-class empleado{
-
-public:
-    empleado(){
-    this->usuario="Octa";
-    this->contra="1234";
-    }
+#include <sstream>
+#include <string>
+#include <vector>
 
 
-    void login (){
-        std::cout<<("Ingresa usuario")<<std::endl;
-        std::cin>>usuariologin;
-        std::cout<<("Ingresa contraseña")<<std::endl;
-        std::cin>>contralogin;
-        if (usuariologin==usuario && contralogin==contra){
-            std::cout<<("Inicio correcto")<<std::endl;
-        }
-        else
-            std::cout<<("Inicio fallido")<<std::endl;
-    }
-
-
-private:
-    std::string usuario;
-    std::string contra;
-    std::string usuariologin;
-    std::string contralogin;
-};
-
-
-
-
-int main (){
-    empleado a;
-    a.login();
-
-
-
-
-
-
-    return 0;
+std::string Login() {
+    std::string usuario, contra;
+    std::cout << "Ingresar usuario para continuar" << std::endl << std:endl << " Usuario: "
+    std::cin >> usuario;
+    std::cout << "Ingresar contraseña para continuar" << std::endl << std:endl << " Usuario: "
+    std::cin >> contra;
 }
+
+    std::string archivoName = "Datos.csv";
+    std::ifstream archivoLectura(archivoName);
+    if (!archivoLectura){
+        std::cout <<"No se ha podido acceder al archivo de la base de datos."
+        return("");
+    }
+    std::string Leer;
+    bool find = false;
+    while(std::string getline(archivoLectura, linea)){
+    std::stringstream ss(linea);
+    std::string campos;
+    std::vector <std::string> campos;
+
+        while(std::getline(ss, campo, ',')){
+            campos.push_back(campo);
+
+        }
+        if(campos.size()>1 && campos[30] ==usuario && campos[30]==contra){
+            find = true;
+            break;
+        }
+    }
+    archivoLectura.close()
+
+if (find){
+std::cout <<"Acceso Correcto."<<std::endl;
+return
+usuario;
+}else{
+    std::cout <<"Acceso Incorrecto."<<std::endl;
+}
+
+
