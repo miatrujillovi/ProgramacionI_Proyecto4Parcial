80ee67bd0b28e8041fe7a307f68cf4110eba98a8
Taverx01 <123278391+Taverx01@users.noreply.github.com>
2024-01-20 23:31:10 -0600

    Arreglar el login y volver a hacerlo funcional

--- MD/SROSM.md
+++ MD/SROSM.md
@@ -2,3 +2,9 @@
 
 ## Proyecto parcial IV
 
+- Cree el archivo login.cpp(lo hice en cpp para poder realizar pruebas de funcionamiento)
+- Realice experimentos utilizando las libreriras "<fstream y sstream para poder crear la base de datos"
+- Cree un funcion que genera y autentitifica la base de datos con datos sencillos(nombre,apellido,contraseña etc)
+- Empece a crear la funcion que nos permitiria logearnos
+- A traves de la funcion login, el programa verifica y compararia los elementos que se encuentran en la columna determinada en el programa, comparandolos con las variables de ingreso que te piden al inicio del codigo, como resultado en caso de que los valores ingresados en las variables de ingreso los cuales son "usuario" y "contraseña" sean iguales a algunos de los valores determinados en el archivo de la base de datos principal, dara entrada al codigo o sencillamente lo cerrara en caso de ser erroneo no ser igual a alguno de los valores establecidos en la base de datos.
+- Agregue una funcion
--- login/login.cpp
+++ login/login.cpp
@@ -4,57 +4,15 @@
 #include <string>
 #include <vector>
 
-// Función que verifica la existencia de la base de datos, y si no, la crea.
-void CreateDB(){
-    std::string filename = "BDPrincipal.csv";
-
-    // Verificamos si existe el archivo
-    std::ifstream fileCheck(filename);
-    if (fileCheck.good()){
-
-    } else {
-        std::ofstream csvFile(filename);
-
-        if (!csvFile.is_open()) {
-            std::cerr << "Error al abrir Base de Datos" << std::endl;
-        }
-
-        // Creamos los encabezados de la base de datos
-        /*std::vector<std::string> headers = {"ID", "Puesto", "Nombre", "Direccion", "EstadoCivil", "RFC", "Salario", "NumeroCuenta", "NumeroSeguroSocial", "FechaContratacion_Dia", "FechaContratacion_Mes", "FechaContratacion_Year", "EstadoEmpresa"};
-        for (size_t i = 0; i < headers.size(); i++) {
-            csvFile << headers[i];
-            if (i < headers_size() -1) {
-                csvFile << ",";
-            }
-        }
-        csvFile << "\n";*/
-        csvFile.close();
+void createDatabaseFile() {
+    std::ifstream file("Datos.csv");
+    if (!file.good()) {
+        std::ofstream outfile("Datos.csv");
+        outfile << "ID,Puesto,Nombre,Direccion,EstadoCivil,RFC,Salario,NumeroCuenta,NumeroSeguroSocial,FechaContratacion_Dia,FechaContratacion_Mes,FechaContratacion_Year,EstadoEmpresa,Usuario,Contraseña\n";
+        outfile.close();
     }
 }
-
-/* Tomado de mi main.cpp, solo usenla para testear que los menús funcionen correctamente
- *
- * Si jala a la primera, soy la reata.
- * Si no jala a la primera... es tu PC, en la mia si corre bien :D
- *
- * */
-
-void TestMenu() {
-    int option;
-
-    std::cout << "[DEBUG] Escoge un menu" << std::endl;
-    std::cout << "[1] Menu gerente" << std::endl;
-    std::cout << "[2] Menu jefe" << std::endl;
-
-    std::cin >> option;
-
-    if(option == 1) {
-        MenuGerente();
-    } else if(option == 2) {
-        MenuJefe();
-    }
-}
-
+int rango;
 std::string Login() {
     std::string usuario, contra;
     std::cout << "Ingresar usuario para continuar" << std::endl << " Usuario: ";
@@ -80,8 +38,24 @@ std::string Login() {
             campos.push_back(campo);
         }
 
-        if (campos.size() >= 2 && campos[0] == usuario && campos[4] == contra) {
+        if (campos.size() >= 2 && campos[13] == usuario && campos[14] == contra) {
             find = true;
+            if(campos.size()>=2 && campos[1]=="jefe"||campos[1]=="Jefe"){
+                rango=0;
+                break;}
+            if(campos.size()>=2 && campos[1]=="gerente"||campos[1]=="Gerente"){
+                rango=1;
+                break;}
+            if(campos.size()>=2 && campos[1]=="contador"||campos[1]=="Contador"){
+                rango=2;
+                break;}
+            if(campos.size()>=2 && campos[1]=="almacenes"||campos[1]=="Almacenes"){
+                rango=3;
+                break;}
+            if(campos.size()>=2 && campos[1]=="empleado"||campos[1]=="Empleado"){
+                rango=4;
+                break;}
+
             break;
         }
     }
@@ -89,7 +63,9 @@ std::string Login() {
 
     if (find) {
         std::cout << "Acceso Correcto." << std::endl;
+        std::cout<<rango;
         return usuario;
+
     } else {
         std::cout << "Acceso Incorrecto." << std::endl;
         return "";
