caef6cb0c29c35d277f6298c2b2c40635ea800b9
Taverx01 <123278391+Taverx01@users.noreply.github.com>
2024-01-21 22:50:30 -0600

    actualizar

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/sdedf/CLionProjects/ProgramacionI_Proyecto4Parcial/df
+# Install script for directory: C:/Users/sdedf/CLionProjects/ProgramacionI_Proyecto4Parcial/dfafg
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/sdedf/CLionProjects/ProgramacionI_Proyecto4Parcial/df/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/sdedf/CLionProjects/ProgramacionI_Proyecto4Parcial/dfafg/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
+
 #include "login.h"
 
 
